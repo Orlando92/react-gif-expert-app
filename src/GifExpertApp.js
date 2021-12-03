6b8915492a715dafecb8f62b1c12e07ddb882d8a
Orlando Heredia <oheredia20122063@gmail.com>
2021-12-03 14:08:47 -0500

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/CategoriesAdd';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map(cat => <GifGrid key={cat} category={cat} />)
+                }
+            </ol>
+        </>
+    );
+
+
+}
